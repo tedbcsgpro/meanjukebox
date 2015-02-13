2c1fcc973cba67ec147b4c5ec1da00b3c6939bdb
tedbcsgpro <tedb@csgpro.com>
2015-02-12 23:44:05 -0800

    PiTunes, changed font color to black, folder names in audioFiles.js

--- MEANJukeBox/Api/audioFiles.js
+++ MEANJukeBox/Api/audioFiles.js
@@ -1,23 +1,21 @@
-﻿
-
-// GetFiles
+﻿// GetFiles
 // generates a flattened array of parent-child Folder -> Songs objects for any given nested set of audio files in a file structure
 var audioFiles = function () {
     
+    
     var fs = require('fs');
     var self = this;
-    var AUDIO_DIRPATH = '';
-   
-
-    self.getFiles = function (path) {
-        AUDIO_DIRPATH = path;
-        var files = getFiles_(path);
-        //var ret = { files: [] };
-        //ret.files.push(files);
-        return files;
+    
+    self.getFiles = function (audioFolderPath, audioBaseUrl) {
+        if (!audioFolderPath || !audioBaseUrl) {
+            throw new Error('audioFiles.getFiles method requires audioFolderPath and audioBaseUrl arguments');
+        }
+        self.AUDIO_FOLDERPATH = audioFolderPath;
+        self.AUDIO_BASEURL = audioBaseUrl;
+        return getFiles_(self.AUDIO_FOLDERPATH);
     };
     
-    var getFiles_ = function(folder, files_, parentFolder_) {
+    var getFiles_ = function (folder, files_, parentFolder_) {
         var hasFiles = false, 
             name = '';
         
@@ -37,29 +35,24 @@ var audioFiles = function () {
         }
         
         if (hasFiles) {
-            var arNames = folder.split("/")
-            name = arNames[arNames.length - 1];
-            name = name.replace('/', '-');
-            parentFolder_ = { folderPath: folder.replace(AUDIO_DIRPATH,''), folderTitle: name, songs: [] };
+            name = folder.replace((self.AUDIO_FOLDERPATH + '/'), '');
+            name = name.replace('/', ' ')
+            parentFolder_ = { folderPath: folder.replace(self.AUDIO_FOLDERPATH, ''), folderTitle: name, songs: [] };
             files_.push(parentFolder_);
         }
         
         for (var i in files) {
-            
             name = folder + '/' + files[i];
-            
+            songUrl = folder.replace(self.AUDIO_FOLDERPATH, self.AUDIO_BASEURL) + '/' + files[i];
             if (fs.statSync(name).isDirectory()) {
                 getFiles_(name, files_);
             } else {
-                parentFolder_.songs.push({ songTitle: files[i].replace('.mp3',''), songFile:files[i]});
+                parentFolder_.songs.push({ songTitle: files[i].replace('.mp3', ''), songFile: songUrl });
             }
         }
         
         return files_;
     };
- 
-   
-
 };
 
 module.exports = audioFiles;
