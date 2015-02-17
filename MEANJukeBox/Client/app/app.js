e686f7ae1a09e20c5e12f41a259c29850ce4f2ea
tedbcsgpro <tedb@csgpro.com>
2015-02-16 19:32:10 -0800

    updated single controller, pre-audio directive cleanup, workinfg

--- MEANJukeBox/Client/app/app.js
+++ MEANJukeBox/Client/app/app.js
@@ -9,44 +9,54 @@
     .factory('httpService', ['$http', HTTPService]);
     
     
-    FoldersCtrl.$inject = ['$scope', '$http', 'httpService'];
-    function FoldersCtrl($scope, $http, httpService) {
+    FoldersCtrl.$inject = ['$scope', '$http', '$timeout', 'httpService'];
+    function FoldersCtrl($scope, $http, $timeout, httpService) {
 
         var self = this;
         self.restUrl = "metated.net/api/audiofiles",
         self.currentFolder,
         self.currentSong,
         self.player = document.getElementById('audioplayer');
+        self.audiomarquis= document.getElementById('audiomarquis');
         
         angular.element(self.player).on('ended', ended);
 
         httpService.getSongList(self.restUrl).then(function (data){
             self.folders = data;
+            window.songdata = data;
         });
         
         self.playAll = function (folder) {
             self.currentFolder = folder;
-            self.currentSong = folder.songs[0].songFile;
-            self.player.src = self.currentSong;
-            self.player.play();
+            self.playSong(folder.songs[0]);
         };
 
         self.playSong = function (song) {
-            self.currentSong = song.songFile;
-            self.player.src = self.currentSong;
+            self.currentSong = song;
+            self.currentFolder = findFolderBySong(self.folders, self.currentSong)
+            self.player.src = self.currentSong.songFile;
             self.player.play();
+            self.audiomarquis.innerText = self.currentSong.songTitle;
         };
 
         function ended(event, args) {
-            if (self.currentFolder) {
-                
-
-            }
+            $timeout(function (event, args) {
+                var curIdx,
+                    numSongs;
+                if (self.currentFolder) {
+                    numSongs = self.currentFolder.songs.length-1;
+                    curIdx = findIndexByKeyValue(self.currentFolder.songs, "songTitle", self.currentSong.songTitle);
+                    if ((curIdx + 1) < numSongs) {
+                       self.playSong(self.currentFolder.songs[curIdx+1])
+                    } else {
+                        self.playAll(self.currentFolder);
+                    }
+                }
+            
+            },10);
         }
 
     }
-
-    
 
     function HTTPService($http) {
         
@@ -60,6 +70,26 @@
         return factory;
     }
 
+    function findIndexByKeyValue(obj, key, value) {
+        for (var i = 0; i < obj.length; i++) {
+            if (obj[i][key] == value) {
+                return i;
+            }
+        }
+        return null;
+    }
  
+    function findFolderBySong(folderCollection, song) {
+        for (var i = 0; i < folderCollection.length; i++) {
+            var songs = folderCollection[i].songs
+            for (var t = 0; t < songs.length;t++)
+                {
+                     if (folderCollection[i].songs[t] === song) {
+                        return folderCollection[i];
+                    }
+                }
+        }
+        return null;
+    }
 })();
 
