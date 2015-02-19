9b87cf8dc731aa24866572c57eb30701ebe5c002
tedbcsgpro <tedb@csgpro.com>
2015-02-19 14:17:56 -0800

    audiodirective complete, playlist still template

--- MEANJukeBox/Client/app/app.js
+++ MEANJukeBox/Client/app/app.js
@@ -21,40 +21,43 @@
                 console.log($scope);
                 var self = this;
                 self.song,
+                self.songIdx = 0,
                 self.folder,
-                self.currentSong,
                 self.playNextSong = true; // TODO: bind to checkbox, default to checked, to allow specifying playing next song in list
                 
                 $scope.ended = function (event, args) {
+                    console.log('ended')
                     if (self.playNextSong) {
-
+                        var newIdx = self.songIdx + 1;
+                        if (newIdx < self.folder.songs.length - 1) {
+                            $timeout(function () {
+                                self.song = self.folder.songs[newIdx]
+                                self.songIdx = newIdx;
+                                $scope.$broadcast('song-changed', { song: self.song, folder: self.folder });
+                            },0)
+                            
+                        }
                     }
                 };
 
                 $scope.$on('play-song', function (event, args) {
-                    if (args.folder) {
+                    if (args.folder) { 
                         self.folder = args.folder;
                         if (args.song) {
                             self.song = args.song;
                         } else {
                             self.song = folder.songs[0];
                         }
+                        self.songIdx = helperServices.findIndexByKeyValue(self.folder.songs, "songTitle", self.song.songTitle);
                     }
                 });
-
-                
 
             },
             controllerAs:'playerVM',
             link: function (scope, iElem, iAttrs) {
                 console.log('link');
-                //console.log(scope);
                 scope.player = angular.element(iElem.children()[iElem.children().length - 1]);
                 scope.player.on('ended', scope.ended);
-                //player.on('ended', function (event, args) {
-                 //   scope.$broadcast('song-ended');
-                //});
-                //scope.player = player[0];
             }
         }
     } 
@@ -77,8 +80,6 @@
             self.folders = data;
         });
         
-        angular.element(self.player).on('ended', ended);
-
         self.playAll = function (folder) {
             self.currentFolder = folder;
             $scope.$emit('play-song', {song:null, folder:folder})
@@ -90,26 +91,12 @@
             $scope.$emit('play-song', { song:self.currentSong, folder:self.currentFolder })
         };
 
-        function ended(event, args) {
-            $timeout(function (event, args) {
-                var curIdx,
-                    numSongs;
-                if (self.currentFolder) {
-                    numSongs = self.currentFolder.songs.length-1;
-                    curIdx = helperServices.findIndexByKeyValue(self.currentFolder.songs, "songTitle", self.currentSong.songTitle);
-                    if ((curIdx + 1) < numSongs) {
-                       self.playSong(self.currentFolder.songs[curIdx+1])
-                    } else {
-                        self.playAll(self.currentFolder);
-                    }
-                }
-            
-            },0);
-        }
-
+        $scope.$on('song-changed', function (event, args) {
+            self.currentSong = args.song;
+            self.currentFolder = args.folder;
+        });
     }
     
-  
     
     function HTTPService($http) {
         
